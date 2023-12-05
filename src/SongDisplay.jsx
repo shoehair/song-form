b1ad8ea5244cb71c33974ec64a36736375d5c94e
Isabella Borkovic <ibork@Isabellas-MacBook-Pro-3.local>
2023-12-05 15:45:31 -0800

    added randomness

--- src/SongDisplay.jsx
+++ src/SongDisplay.jsx
@@ -9,10 +9,11 @@ export default function SongDisplay({
   artist,
 })
 {
+  let randomNumber = Math.random();
+  console.log(randomNumber);
   let arrow = "<=";
-  return (
-    <>
-    <div className="main-container"> Both songwriters were asked to write a song in the style of {artist}. </div>
+
+  let songDiv = (
     <div className="song-display-container">
       <div className="song-item">{songOne}</div>
       <button onClick={handlePreference(index, "llama")}>
@@ -21,6 +22,22 @@ export default function SongDisplay({
       <div className="song-item">{songTwo}</div>
       <button onClick={handlePreference(index, "lyre")}> {arrow} Prefer Song Two</button>
     </div>
+  )
+  if (randomNumber < 0.5) {
+    songDiv = <div className="song-display-container">
+    <div className="song-item">{songTwo}</div>
+    <button onClick={handlePreference(index, "lyre")}> {arrow} Prefer Song Two</button>
+    <div className="song-item">{songOne}</div>
+    <button onClick={handlePreference(index, "llama")}>
+      {arrow}Prefer Song One
+    </button>
+  </div>
+  }
+
+  return (
+    <>
+    <div className="main-container"> Both songwriters were asked to write a song in da style of {artist}. </div>
+    {songDiv}
   </>
   );
 }
